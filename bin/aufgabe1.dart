acce61d5b78e9b88cab2e42449016192b431dcec
Daniel Trompetter <danieltrompetter@gmail.com>
2025-08-13 11:27:04 +0200

    init

---
+++ bin/aufgabe1.dart
@@ -0,0 +1,5 @@
+import 'package:aufgabe1/aufgabe1.dart' as aufgabe1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${aufgabe1.calculate()}!');
+}
---
+++ lib/aufgabe1.dart
@@ -0,0 +1,15 @@
+  void main() {
+    String gdStr = "Guten";
+    String dayStr ="Tag";
+    String worldStr = "Welt";
+    int d = 20;
+    String timeStr = "time";
+    int seven = 7;
+    int twelve = 12;
+    String exclamationStr = "!";
+    int a = 24;
+    String beautyStr = "Schöne";
+    String morningStr ="Morgen";
+    String todayStr = "Heute";
+    String spaceStr = " ";
+   }
